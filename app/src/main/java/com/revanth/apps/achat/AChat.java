127551baa9b85f029d828e0f1c0f6912422eb58d
SHRAVANI <patthishravani@gmail.com>
2019-02-14 22:54:56 +0530

    Part 27( has some issues)

--- app/src/main/java/com/revanth/apps/achat/AChat.java
+++ app/src/main/java/com/revanth/apps/achat/AChat.java
@@ -9,6 +9,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
@@ -44,7 +45,7 @@ public class AChat extends Application {
                     Log.d("Rocky", "achat online msg2");
                     if (dataSnapshot != null) {
                         Log.d("Rocky", "achat online msg3");
-                        mUserDatabase.child("online").onDisconnect().setValue(false);
+                        mUserDatabase.child("online").onDisconnect().setValue(ServerValue.TIMESTAMP);
 
                     }
                 }
