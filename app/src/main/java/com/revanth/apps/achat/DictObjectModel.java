f3475eedeee10658528f033601844632b4ee3473
Shravani <patthishravani@gmail.com>
2019-02-28 17:30:21 +0530

    Dictionary done

---
+++ app/src/main/java/com/revanth/apps/achat/DictObjectModel.java
@@ -0,0 +1,24 @@
+package com.revanth.apps.achat;
+
+public class DictObjectModel {
+    String word, meaning;
+
+    public DictObjectModel(String word, String meaning){
+
+        this.word=word;
+        this.meaning = meaning;
+
+
+    }
+    public  String getWord()
+    {
+        return word;
+    }
+
+    public  String getMeaning()
+    {
+        return meaning;
+    }
+
+
+}
