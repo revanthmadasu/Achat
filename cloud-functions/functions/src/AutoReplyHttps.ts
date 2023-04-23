dbdddd014e5a81052c7c05a6ba0225b563ec2e59
Revanth Madasu <revanthmadasu913@gmail.com>
2023-04-23 18:32:31 -0500

    fix: continuous auto replies deleting cloud task in auto reply api

--- cloud-functions/functions/src/AutoReplyHttps.ts
+++ cloud-functions/functions/src/AutoReplyHttps.ts
@@ -1,9 +1,25 @@
 import { logger, https } from "firebase-functions";
 import { autoReply } from "./AutoReply";
+import { CloudTasksService } from "./CloudTasks";
 
 export const autoReplyApi = https.onRequest((req, res) => {
     logger.log("Received api call on autoreply api");
     const { body } = req;
+    const projectId = req.get('X-CloudTasks-ProjectId');
+    const queueName = req.get('X-CloudTasks-QueueName');
+    const taskName = req.get('X-CloudTasks-TaskName');
+
+    logger.info(`project id from req: ${projectId}`);
+    logger.info(`queueName from req: ${queueName}`);
+    logger.info(`taskName from req: ${taskName}`);
+
+    const cloudTasksService = CloudTasksService.getCloudTasksService();
+
+    cloudTasksService.deleteTask(taskName || "").then((res) => {
+        logger.log("Successfully deleted task. res: ", res);
+    }).catch(error => {
+        logger.error("Error in deleting task: ", JSON.stringify(error));
+    });
     logger.info(`Received body: ${body}`);
     try {
         logger.info(`Received body: ${JSON.stringify(body)}`);
