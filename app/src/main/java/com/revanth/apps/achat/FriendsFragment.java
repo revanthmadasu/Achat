b288cb0107f7f82a5d0d03cfd7d23edb387b0bff
Revanth Madasu <revanthmadasu913@gmail.com>
2023-04-30 18:58:19 -0500

    chore: added picasso

--- app/src/main/java/com/revanth/apps/achat/FriendsFragment.java
+++ app/src/main/java/com/revanth/apps/achat/FriendsFragment.java
@@ -20,7 +20,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
-
+import com.squareup.picasso.Picasso;
 /**
  * A simple {@link Fragment} subclass.
  */
--- app/src/main/java/com/revanth/apps/achat/UsersActivity.java
+++ app/src/main/java/com/revanth/apps/achat/UsersActivity.java
@@ -23,6 +23,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 
+import com.squareup.picasso.Picasso;
 public class UsersActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
